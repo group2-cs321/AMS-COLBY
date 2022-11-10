58c244d9db62bce26dce458bb5635c030b09a275
tymkiwdylan <datymk23@colby.edu>
2022-11-09 19:02:15 -0500

    works

--- website/__init__.py
+++ website/__init__.py
@@ -18,12 +18,7 @@ def create_app():
    # local development, where we'll use environment variables
         print("Loading config.development and environment variables from .env file.")
         app.config['SECRET_KEY'] = 'secret-key-goes-here'
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-                                                    dbuser=os.environ['DBUSER'],
-                                                    dbpass=os.environ['DBPASS'],
-                                                    dbhost=os.environ['DBHOST'] + ".postgres.database.azure.com",
-                                                    dbname=os.environ['DBNAME']
-                                                )
+        app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     else:
    # production
         print("Loading config.production.")
@@ -48,7 +43,8 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User
-    # create_database(app)
+    if not 'WEBSITE_HOSTNAME' in os.environ:
+        create_database(app)
 
     
     loging_manager = LoginManager()
